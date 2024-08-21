8fc88c3d72c6bee8e4b070623b0fcd6810861265
anmolkhuwaja <anmolkhuwaja.xwave@gmail.com>
2024-08-21 06:54:44 -0700

    Modal Pop Up

---
+++ MODAL/script.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById('myModal');
+let closeModal = document.getElementById('closeModal');
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+function closed(){
+    modal.style.display = "none";
+}
